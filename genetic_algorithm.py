cb24d446d9b8b0d70659ab7f34d5bbe6503c5184
Stadnik Andrii <stadnikandriy1@gmail.com>
2020-05-28 14:58:42 +0300

    Update stuff

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -7,51 +7,41 @@ from copy import copy
 from statistics import mean
 
 import numpy as np
-import pytesseract
 from tqdm import tqdm, trange
 import pickle
 
-from text_item import TextItem
 from multiprocessing.pool import Pool
 from multiprocessing import cpu_count
+from mnist.mnist import pred
+import plotly.express as px
+import plotly.io as pio
+pio.renderers.default = 'browser'
 
 
-def score(ti: TextItem, c=6) -> int:
-    """
-    Compute the score for the image
-
-    The score is the confidence of the recognized text
-
-    :param img TextItem: Input text item with image
-    :param c int: psm argument for tesseract
-    :rtype int: score
-    """
-    img = ti.get_pil()
-    d_ = pytesseract.image_to_data(
-        img, output_type=pytesseract.Output.DICT, config=f'--psm {c} --oem 0')
-    # Add normalization by noise sum
-    return max([c for c in d_['conf'] if isinstance(c, int)] + [0])
-
-
-def crossover(f: TextItem, m: TextItem, mr: float) -> TextItem:
+def crossover(n_1, n_2, mr: float):
     """
     Crossover for genetic algorithm.
 
     This function performs crossover for given mother and father and mutation
     for the child after
 
-    :param f TextItem: Father
-    :param m TextItem: Mother
+    :param n_1 TextItem: Father
+    :param n_2 TextItem: Mother
     :param mr float: mutation rate
     :rtype TextItem: Child
     """
     # Probably should resize here
-    child = copy(f)
+    # child = copy(f)
+    s = n_1.shape
+    child = np.empty(s)
     b_opts = [True, False]
-    s = f.noise.shape
-    child.noise = np.where(np.random.choice(b_opts, size=s), f.noise, m.noise)
+    child = np.where(np.random.choice(b_opts, size=s), n_1, n_2)
+    # child = (n1 + n2) / 2
     mask = np.random.choice(b_opts, size=s, p=((1 - mr), mr))
-    child.noise[mask] = np.random.randint(0, 10, size=s, dtype='uint8')[mask]
+    # __import__('ipdb').set_trace()
+    child[mask] = (np.random.rand(*s) / 5 - 0.1)[mask]
+    child = np.minimum(np.full(child.shape, 0.2), child)
+    child = np.maximum(np.full(child.shape, -0.2), child)
     return child
 
 
@@ -60,57 +50,67 @@ class GeneticAlgorithm():
     This is the class which implements the genetic algorithm.
     """
 
-    def __init__(self, ti: TextItem, pool_size: int = 1000, mutation_rate: float
-                 = 0.0001, n_iter: int = int(1e6)):
+    def __init__(self, img, target=0, n_of_elite: int = 2, pool_size: int = 1000,
+                 mutation_rate: float = 0.01, n_iter: int = int(1e6)):
         self.pool_size = pool_size
+        self.target = target
         self.mutation_rate = mutation_rate
         self.n_iter = n_iter
-        self.pool = []
         self.scores = None
-        self.setup(ti)
+        self.img = img
+        self.n_of_elite = n_of_elite
+        self.needed_iterations = None
+        self.pool = [np.random.rand(*img.shape) / 5 - 0.1
+                     for _ in range(pool_size)]
 
-    def run(self):
-        with trange(self.n_iter) as t:
-            for i in t:
-                self.comp_score()
-                if min(self.scores) == 0:
-                    tqdm.write(
-                        f'Got 0 confidence at {i} iteration')
-                    return self.pool[np.argmin(self.scores)]
-                t.set_description(
-                    f'Score: mean = {mean(self.scores)}, min = {min(self.scores)}')
-                self.evolve()
-                if not i % 10:
-                    tqdm.write(
-                        f'Score at {i} iteration: mean = {mean(self.scores)}, min = {min(self.scores)}')
-                    with open('pool.pickle', 'wb') as f:
-                        pickle.dump(self.pool, f)
+    def finish(self, i):
+        self.iterations_needed = i
+        self.best_confidence = max(self.scores)
+        # return self.img + self.pool[np.argmax(self.scores)]
+        return self.iterations_needed, self.best_confidence, self.img + self.pool[np.argmax(self.scores)]
 
-    def setup(self, ti: TextItem):
-        for _ in range(self.pool_size):
-            c = copy(ti)
-            c.noise = np.random.randint(0, 10, size=c.img.shape, dtype='uint8')
-            self.pool.append(c)
+    def run(self, v=False):
+        scores = []
+        for i in (t := trange(self.n_iter, disable=not v)):
+            self.comp_score(v=v)
+            if max(self.scores) > 0.95:
+                t.write(f'Got {max(self.scores)} confidence at {i} iteration')
+                return self.finish(i)
+            t.set_description(
+                f'Score: mean = {mean(self.scores)}, '
+                f'max = {max(self.scores)}, std = {np.std(self.scores)}')
+            self.evolve()
+            if not i % 10 and v:
+                t.write(f'Score at {i} iteration: mean = {mean(self.scores)}, '
+                        f'max = {max(self.scores)}, std = {np.std(self.scores)}')
+                # with open('pool.pickle', 'wb') as f:
+                #     pickle.dump(self.pool, f)
+            scores.append(mean(self.scores))
+            if i and not i % 100:
+                px.line(y=scores).show()
+        return self.finish(i)
 
-    def comp_score(self):
-        n_cpu = cpu_count() - 4
-        with Pool(n_cpu, initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),)) as p:
-            self.scores = list(tqdm(
-                p.imap(score, self.pool),
-                desc=f'Computing scores with {n_cpu} cores',
-                total=len(self.pool), leave=False))
+    def comp_score(self, v=True):
+        """
+        Compute the confidence of the recognized text for the images
+        """
+        self.scores = [r[self.target]
+                       for r in pred(self.img + self.pool, v)]
 
     def evolve(self):
         pool = []
-        scores = (100 - np.array(self.scores)) ** 3
+        # scores = (100 - np.array(self.scores)) ** 3
+        scores = self.scores
         scores = scores / sum(scores)
-        n_min_to_save = 20
-        for _ in trange(self.pool_size - n_min_to_save, desc='Updating pool', leave=False):
-            f, m = np.random.choice(self.pool, size=2, replace=False,
+        # for _ in trange(self.pool_size - n_min_to_save, desc='Updating pool', leave=False):
+        for _ in range(self.pool_size - self.n_of_elite):
+            f, m = np.random.choice(range(self.pool_size), size=2, replace=False,
                                     p=scores)
+            f, m = self.pool[f], self.pool[m]
             child = crossover(f, m, self.mutation_rate)
             pool.append(child)
         # pool.extend(sorted(self.pool, key = lambda ti: ti.score)[:20])
-        pool.extend([self.pool[i] for i in np.argsort(self.scores)[:20]])
+        pool.extend([self.pool[i]
+                     for i in np.argsort(scores)[-self.n_of_elite:]])
         assert (len(pool) == self.pool_size)
         self.pool = pool
--- main.py
+++ main.py
@@ -3,27 +3,62 @@
 
 """This is the main module"""
 
-from helpers import parse_args
-from img_ocr import ImgOCR
-from text_item import TextItem
-from genetic_algorithm import GeneticAlgorithm
-from ipdb import launch_ipdb_on_exception
+from functools import partial
+from tqdm import tqdm
+import plotly.express as px
+import plotly.io as pio
+# from ipdb import launch_ipdb_on_exception
+import numpy as np
+import pickle
 
+from genetic_algorithm import GeneticAlgorithm
+from mnist.mnist import generate_data
+from multiprocessing.pool import Pool
+from multiprocessing import cpu_count
+
+pio.renderers.default = 'browser'
+
+def pred(kwargs):
+    v = kwargs.pop('v', None)
+    return GeneticAlgorithm(**kwargs).run(v=v)
 
 def main():
     """Main function"""
-    args = parse_args()
+    inputs, targets, reals = generate_data(1)
 
-    img = ImgOCR(path=args.image)
-    img.resize((1280, -1))
-    data = img.compute_text_data()
-    # img.show(annotate=True)
+    # d = data[1]
+    # px.imshow(inputs[0].reshape(28, 28)).show()
+    params = []
+    img = inputs[0]
+    target = np.nonzero(targets[0])[0][0]
+    # for pool_size in 2 ** np.arange(2, 12, step=3):
+    #     for mutation_rate in [0.3, 0.1, 0.05, 0.01]:
+    #         for n_of_elite in [1, 3]:
+    for pool_size in 2 ** np.arange(2, 6, step=1):
+        for mutation_rate in [0.3]:
+            for n_of_elite in [1]:
+                params.append({'img': img,
+                               'target': target,
+                               'pool_size': pool_size,
+                               'mutation_rate': mutation_rate,
+                               'n_of_elite': n_of_elite,
+                               'n_iter': 2,
+                               'v': False})
 
-    d = data[1]
-    model = GeneticAlgorithm(d)
-    model.run()
+    with Pool(cpu_count(), initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),)) as p:
+        rez = list(tqdm(p.imap(pred, params), total=len(params)))
+
+    rez = [{**{k: v for k, v in p.items() if k not in {'img', 'target', 'v'}},
+            **{'iterations_needed': r[0], 'best_confidence': r[1]}}
+            for p, r in zip(params, rez)]
+
+    with open('rezults.pickle', 'wb') as f:
+        pickle.dump(rez, f)
+
+    # model = GeneticAlgorithm(inputs[0], np.nonzero(targets[0])[0][0], n_iter=10)
+    # rez = model.run(v=True)
+    # px.imshow(rez.reshape(28, 28)).show()
 
 
 if __name__ == "__main__":
-    with launch_ipdb_on_exception():
-        main()
+    main()
